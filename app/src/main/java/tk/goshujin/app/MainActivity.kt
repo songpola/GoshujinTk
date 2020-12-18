535c86b514eb8e75bd71653f621c0a4098bc2a60
Songpol Anannetikul <marstherice@hotmail.com>
2020-12-18 16:22:11 +0700

    Fix View Binding

--- app/src/main/java/tk/goshujin/app/MainActivity.kt
+++ app/src/main/java/tk/goshujin/app/MainActivity.kt
@@ -2,15 +2,14 @@ package tk.goshujin.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import tk.goshujin.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ConstraintLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).root
-        setContentView(binding)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
