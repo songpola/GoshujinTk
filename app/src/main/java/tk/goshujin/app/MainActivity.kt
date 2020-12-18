87ffa9d6acc203fa536c901691ff9a402516da73
Songpol Anannetikul <marstherice@hotmail.com>
2020-12-18 16:10:05 +0700

    Use View Binding

--- app/src/main/java/tk/goshujin/app/MainActivity.kt
+++ app/src/main/java/tk/goshujin/app/MainActivity.kt
@@ -2,10 +2,15 @@ package tk.goshujin.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import tk.goshujin.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).root
+        setContentView(binding)
     }
 }
